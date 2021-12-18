f3ebfd8ee11ef9e873b94e23b30b021c876f528a
Justin Baker <me@jbaker.dev>
2021-12-17 17:54:50 -0800

    Boilerplate: Customize tailwind
      -  Support components
      - Add colors

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -3,11 +3,34 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   darkMode: 'media',
   content: [
+    './app/components/**/*',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*'
   ],  
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current:   'currentColor',
+      black:     '#000000',
+      base03:    '#002b36',
+      base02:    '#073642',
+      base01:    '#586e75',
+      base00:    '#657b83',
+      base0:     '#839496',
+      base1:     '#93a1a1',
+      base2:     '#eee8d5',
+      base3:     '#fdf6e3',
+      white:     '#ffffff',
+      yellow:    '#b58900',
+      orange:    '#cb4b16',
+      red:       '#dc322f',
+      magenta:   '#d33682',
+      violet:    '#6c71c4',
+      blue:      '#268bd2',
+      cyan:      '#2aa198',
+      green:     '#859900',
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
