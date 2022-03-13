c87d18f6a29553117fd5366b06c140852863fcdd
Justin Baker <me@jbaker.dev>
2022-03-13 09:14:55 -0700

    Enable lookbook for all environments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root to: "home#index"
 
-  if Rails.env.development?
-    mount Lookbook::Engine, at: "/lookbook"
-  end
+  mount Lookbook::Engine, at: "/lookbook"
 end
