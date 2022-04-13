80f6abb87fc52241c5d23ae56ca839cfd8503de2
Justin Baker <me@jbaker.dev>
2022-04-13 14:14:44 -0700

    Use cookies/session to set theme

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :set_theme
+
+  def set_theme
+    session[:user_theme] = cookies[:user_theme] ||= "dark"
+  end
 end
--- app/javascript/controllers/theme_controller.js
+++ app/javascript/controllers/theme_controller.js
@@ -4,13 +4,22 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   toggle() {
     let html = document.documentElement;
+    let theme = null;
 
     if (html.classList.contains('theme-light')) {
       html.classList.remove('theme-light');
       html.classList.add('theme-dark');
+      theme = "dark";
     } else if (html.classList.contains('theme-dark')) {
       html.classList.remove('theme-dark');
       html.classList.add('theme-light');
+      theme = "light";
     }
+
+    this.set_cookie(theme);
+  }
+
+  set_cookie(theme) {
+    document.cookie = "user_theme=" + theme;
   }
 }
