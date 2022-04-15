df5da54a1416bddfa8d5d5c0d0d5a74c88a73215
Justin Baker <me@jbaker.dev>
2022-04-14 17:03:07 -0700

    Add switchable light/dark theme (#17)
    * Setup invertible color scheme
    
      - Using CSS variables a la https://tailwindcss.com/docs/customizing-colors#using-css-variables
    
    * Add theme toggle
    
    * Use cookies/session to set theme

---
+++ app/javascript/controllers/theme_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="theme"
+export default class extends Controller {
+  toggle() {
+    let html = document.documentElement;
+    let theme = null;
+
+    if (html.classList.contains('theme-light')) {
+      html.classList.remove('theme-light');
+      html.classList.add('theme-dark');
+      theme = "dark";
+    } else if (html.classList.contains('theme-dark')) {
+      html.classList.remove('theme-dark');
+      html.classList.add('theme-light');
+      theme = "light";
+    }
+
+    this.set_cookie(theme);
+  }
+
+  set_cookie(theme) {
+    document.cookie = "user_theme=" + theme;
+  }
+}
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,5 +1,15 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
+// Cribbed from: https://tailwindcss.com/docs/customizing-colors#using-css-variables
+function withOpacityValue(variable) {
+  return ({ opacityValue }) => {
+    if (opacityValue === undefined) {
+      return `rgb(var(${variable}))`
+    }
+    return `rgb(var(${variable}) / ${opacityValue})`
+  }
+}
+
 module.exports = {
   darkMode: 'media',
   content: [
@@ -11,25 +21,34 @@ module.exports = {
   theme: {
     colors: {
       transparent: 'transparent',
-      current:   'currentColor',
-      black:     '#000000',
-      base03:    '#002b36',
-      base02:    '#073642',
-      base01:    '#586e75',
-      base00:    '#657b83',
-      base0:     '#839496',
-      base1:     '#93a1a1',
-      base2:     '#eee8d5',
-      base3:     '#fdf6e3',
-      white:     '#ffffff',
-      yellow:    '#b58900',
-      orange:    '#cb4b16',
-      red:       '#dc322f',
-      magenta:   '#d33682',
-      violet:    '#6c71c4',
-      blue:      '#268bd2',
-      cyan:      '#2aa198',
-      green:     '#859900',
+      current:     'currentColor',
+
+      // Solarized Palette
+      black:       withOpacityValue('--color-black'),
+      base03:      withOpacityValue('--color-base03'),
+      base02:      withOpacityValue('--color-base02'),
+      base01:      withOpacityValue('--color-base01'),
+      base00:      withOpacityValue('--color-base00'),
+      base0:       withOpacityValue('--color-base0'),
+      base1:       withOpacityValue('--color-base1'),
+      base2:       withOpacityValue('--color-base2'),
+      base3:       withOpacityValue('--color-base3'),
+      white:       withOpacityValue('--color-white'),
+      yellow:      withOpacityValue('--color-yellow'),
+      orange:      withOpacityValue('--color-orange'),
+      red:         withOpacityValue('--color-red'),
+      magenta:     withOpacityValue('--color-magenta'),
+      violet:      withOpacityValue('--color-violet'),
+      blue:        withOpacityValue('--color-blue'),
+      cyan:        withOpacityValue('--color-cyan'),
+      green:       withOpacityValue('--color-green'),
+
+      // Dynamic Palette
+      solar1:      withOpacityValue('--color-solar1'),
+      solar2:      withOpacityValue('--color-solar2'),
+      solar3:      withOpacityValue('--color-solar3'),
+      solar4:      withOpacityValue('--color-solar4'),
+      solar5:      withOpacityValue('--color-solar5'),
     },
     extend: {
       fontFamily: {
