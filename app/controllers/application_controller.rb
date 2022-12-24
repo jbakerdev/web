0cf3552e7f68af42fe6844b3533988eb426f6da4
Justin Baker <me@jbaker.dev>
2022-12-23 16:01:34 -0800

    Set the theme cookie/session separately

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :set_theme
 
   def set_theme
-    session[:user_theme] = cookies[:user_theme] ||= {value: "dark", path: "/"}
+    cookies[:user_theme] ||= {value: "dark", path: "/"}
+    session[:user_theme] = cookies[:user_theme]
   end
 end
