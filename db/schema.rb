c7a17bda24bba1c24da3172280e365744d7906df
Justin Baker <me@jbaker.dev>
2025-09-06 10:10:11 -0700

    Create migration to remove existing users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_09_203621) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_06_165905) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  enable_extension "pg_catalog.plpgsql"
 
 end
