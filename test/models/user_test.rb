f75e384bbec4ba44a381b9319be0dd99f1b48bb3
Justin Baker <me@jbaker.dev>
2025-09-28 12:56:54 -0700

    Build out User model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:jbakerdev)
+  end
+
+  # Validations
+
+  test "valid user" do
+    assert @user.valid?
+  end
+
+  test "invalid without email address" do
+    @user.email_address = nil
+    assert_not @user.valid?, "saved user without an email address"
+    assert_not_nil @user.errors[:email], "no validation errors for email present"
+  end
+
+  test "invalid without password" do
+    @user.password = nil
+    assert_not @user.valid?, "saved user without a password"
+    assert_not_nil @user.errors[:password], "no validation errors for password present"
+  end
+
+  # Normalization
+
+  test "normalize email address" do
+    uppercase = "BOGUS@JBAKER.DEV"
+    @user.email_address = uppercase
+
+    assert @user.email_address, uppercase.downcase
+  end
 end
